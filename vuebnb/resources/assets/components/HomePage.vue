3e5cdb48b1620b20428ad82ae137d0070dcb28e1
Juanma García <juanma.garcia@isobar.com>
2018-02-19 09:17:28 +0000

    Chapter 7. Adding blocks to Home Page

--- vuebnb/resources/assets/components/HomePage.vue
+++ vuebnb/resources/assets/components/HomePage.vue
@@ -1,8 +1,20 @@
 <template>
-  <div>Vuebnb home page</div>
+  <div>
+    <div v-for="(group, country) in listing_groups">
+      <h1>Places in {{ country }}</h1>
+      <div class="listing-summaries">
+        <listing-summary
+        v-for="listing in group"
+        :key="listing.id"
+        :listing="listing"
+        ></listing-summary>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
   import { groupByCountry } from '../js/helpers';
+  import ListingSummary from './ListingSummary.vue';
   
   let serverData = JSON.parse(window.vuebnb_server_data);
   let listing_groups = groupByCountry(serverData.listings);
@@ -10,6 +22,9 @@
   export default {
     data() {
       return { listing_groups }
+    },
+    components: {
+      ListingSummary
     }
   }
 </script>
---
+++ vuebnb/resources/assets/components/ListingSummary.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="listing-summary">
+    {{ listing.address }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['listing'],
+}
+</script>
